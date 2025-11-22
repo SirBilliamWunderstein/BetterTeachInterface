238b0f7caa35104cf76955921b45ec647a7c1fdf
SirBilliamWunderstein <sirbilliamwunderstein@gmail.com>
2025-11-22 18:18:27 +0530

    update 2

--- TheTeacher/GUIpack/Main_Page_Classes.py
+++ TheTeacher/GUIpack/Main_Page_Classes.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from PIL import Image,ImageTk
+import pickle
 
 global fci
 global qnai
@@ -18,6 +19,7 @@ class Main_Page(tk.Tk()):
     global fcB
     global qnaB
     global cbB
+    global canT
     global TITLECARD
     
     def __init__(self):
@@ -29,7 +31,7 @@ class Main_Page(tk.Tk()):
         self.TITLECARD = tk.Label(self,text = "How shalth I assist THEE",fg = "#6a1aa6",bg = "#262626",font = ("Comic Sans","18","bold"),relief = tk.RAISED,borderwidth = 1)
         self.fcB = tk.Button(self,image= fci,fg = "#6a1aa6",bg = "#262626",borderwidth=1,width = 100,height = 300, relief= tk.RIDGE)
         self.qnaB = tk.Button(self,image= qnai,fg = "#6a1aa6",bg = "#262626",borderwidth=1,width = 100,height = 300, relief= tk.RIDGE)
-        self.cbB = tk.Button(self,image= cbi,fg = "#6a1aa6",bg = "#262626",borderwidth=1,width = 100,height = 300, relief= tk.RIDGE)
+        self.cbB = tk.Button(self,image= cbi,command = self.cbC,fg = "#6a1aa6",bg = "#262626",borderwidth=1,width = 100,height = 300, relief= tk.RIDGE)
         self.objB = tk.Button(self,image= obji,fg = "#6a1aa6",bg = "#262626",borderwidth=1,width = 100,height = 300, relief= tk.RIDGE)
         
         self.TITLECARD.place(x =0,y = 0)
@@ -37,12 +39,18 @@ class Main_Page(tk.Tk()):
         self.qnaB.place(x = 120,y = 50)
         self.cbB.place(x = 230,y = 50)
         self.objB.place(x= 340,y = 50)
+    
+    def cbC(self):
+        Q = ChatBoxPg()
+        Q.mainloop()
         
 
 class ChatBoxPg(tk.Tk()):
     global Rcan
     global Qbox
     global can
+    global entry
+    global txt
     
     def __init__(self):
         self.super().__init__()
@@ -54,4 +62,40 @@ class ChatBoxPg(tk.Tk()):
         self.SF2 = tk.Frame(self.SF1,width = 470,height= 320,borderwidth = 3,bg = "#262626",highlightbackground="#6a1aa6", highlightcolor="#6a1aa6",relief = tk.FLAT)
         self.can = tk.Canvas(self.SF2,width = 440,height = 290,scrollregion=(0,0,440,7000),bg = "#262626",fg = "#6a1aa6")
         
+        self.SF1.place(x = 0,y = 50)
+        self.SF2.place(relx = 0.5,rely = 0.5,anchor = tk.CENTER)
+        self.can.place(relx = 0.5,rely = 0.5,anchor = tk.CENTER)
+        self.canT = self.can.create_text((0,0),text = "How shalth i assist THEE")
+        
+        self.yscr = tk.Scrollbar(self.can,orient = "vertical",command=self.can.yview)
+        self.can.config(yscrollcommand=self.can.yscr.set)
+        self.yscr.place(relx=1.0, rely=0, anchor="ne", relheight=1)
+        
+        self.txt = tk.StringVar()
+        self.entry = tk.Entry(self,textvariable=self.txt,width = 500,height = 50,fg = "#6a1aa6",bg = "#262626",font = ("Comic Sans","18","bold"))
+        
+        self.can.bind("<Button-5>",self.upscr)
+        self.can.bind("<Button-4>",self.downscr)
+        self.bind("<Enter>")
+    
+    def Enter_prompt(self):
+        self.can.delete(self.canT)
+        Q = self.txt.get()
+        #function here S returned
+        S = ""
+        self.canT = self.can.create_text((0,0),text = S)
+        Quer = ""
+        #return here
+        SS = "" # fact1:fact2:fact3:fact4
+        binfil = open("../BinDump/" + S + ".dat","wb")
+        SS = SS.split(":")
+        pickle.dump(SS,binfil)
+        binfil.close()        
+    
+    def upscr(self,event):
+        self.yview_scroll(1, "units")
+    
+    def downscr(self,event):
+        self.yview_scroll(-1, "units")
+
         
